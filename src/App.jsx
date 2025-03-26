ede33b91ba7b7d1c928a7f922612a5194ddd7caf
NatalieChurch <nataliechurch12@gmail.com>
2025-03-25 20:40:30 -0500

    Finished everything but last part (render puppy data) and adding css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
 import './App.css'
+// import './path-to-css.css'
 
 function App() {
 
-console.log(puppyList);
+//console.log(puppyList);
 const [puppies, setPuppies] = useState(puppyList);
-console.log(puppies);
+//console.log(puppies);
+const [featPupId, setFeatPupId] = useState(null);
+
 
   return (
     <div className="App">
       {
         puppies.map((puppy) => {
-          return <p>{puppy.name}</p>
+          const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+            console.log(featuredPup)
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })     
       }
+      <p>{featPupId &&  <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>}</p>
     </div>
   )
 }
