3e455d809844c99f63117ed84128a47dcc926cd0
NatalieChurch <nataliechurch12@gmail.com>
2025-03-25 21:11:07 -0500

    Finished!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
 import './App.css'
-// import './path-to-css.css'
 
 function App() {
 
@@ -9,24 +8,24 @@ function App() {
 const [puppies, setPuppies] = useState(puppyList);
 //console.log(puppies);
 const [featPupId, setFeatPupId] = useState(null);
-
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
   return (
     <div className="App">
       {
         puppies.map((puppy) => {
-          const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
             console.log(featuredPup)
           return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })     
       }
-      <p>{featPupId &&  <div>
+      {featPupId &&  <div>
           <h2>{featuredPup.name}</h2>
           <ul>
             <li>Age: {featuredPup.age}</li>
             <li>Email: {featuredPup.email}</li>
           </ul>
-        </div>}</p>
+        </div>}
     </div>
   )
 }
